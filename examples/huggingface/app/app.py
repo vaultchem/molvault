7036343b19b79527e7fd57e97852d50d7c7320e6
janweinreich <janweinreich286@gmail.com>
2024-01-29 17:16:06 +0100

    Update style.css and app.py

--- examples/huggingface/app/app.py
+++ examples/huggingface/app/app.py
@@ -1,9 +1,9 @@
 # Uncomment if run locally
 import os
 
-# import sys
-# sys.path.append(os.path.abspath("../../../molvault"))
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import sys
+sys.path.append(os.path.abspath("../../../molvault"))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from requests import head
 from concrete.ml.deployment import FHEModelClient
