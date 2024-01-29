e18006ee5739ce350fb909d86d85271a0bdf3b14
janweinreich <janweinreich286@gmail.com>
2024-01-29 21:39:23 +0100

    Fix import statement in app.py

--- examples/huggingface/app/app.py
+++ examples/huggingface/app/app.py
@@ -1,6 +1,5 @@
 # Uncomment if run locally
 import os
-
 import sys
 sys.path.append(os.path.abspath("../../../molvault"))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
